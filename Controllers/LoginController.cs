4d612b8649158d3c4762e879655a87f06d51d44b
Casey Lemieux <lemieuxcasey@gmail.com>
2021-02-18 22:48:46 -0500

    Added Test Excel Functionality

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using IDGenWebsite.Models;
+﻿using ExcelDataReader;
+using IDGenWebsite.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -23,15 +25,46 @@ namespace IDGenWebsite.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        //[HttpGet]
+        public IActionResult TestExcel()
+        {
+            return View(new List<StudentModel>());
+        }
+
+        [HttpPost]
+        public IActionResult TestExcel(IFormCollection form)
+        {
+            List<StudentModel> students = new List<StudentModel>();
+
+            var fileName = "./Focus Students Feburary.xlsx";
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            using (var stream = System.IO.File.Open(fileName, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+            {
+                using(var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    reader.Read();
+                    while (reader.Read())
+                    {
+                        students.Add(new StudentModel
+                        {
+                            StudentID = reader.GetValue(0).ToString(),
+                            LastName = reader.GetValue(1).ToString(),
+                            FirstName = reader.GetValue(2).ToString(),
+                            Email = reader.GetValue(3).ToString(),
+                            GradeLevel = reader.GetValue(4).ToString()
+                        });
+                    }
+                }
+            }
+            return View(students);
         }
     }
 }
