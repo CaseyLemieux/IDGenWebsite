42ed3adccc1b609e6c9b9a11582da880cc5d0257
Casey Lemieux <lemieuxcasey@gmail.com>
2021-06-29 15:35:07 -0400

    Login UI Working

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -82,7 +82,7 @@ namespace IDGenWebsite.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
 
                 //Get User and Check Role
                 var user = await _userManager.FindByEmailAsync(Input.Email);
