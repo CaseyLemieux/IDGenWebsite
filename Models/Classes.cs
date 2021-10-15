2ce5beb34596776a7a7b3c68b921fa10919250fb
Casey Lemieux <lemieuxcasey@gmail.com>
2021-10-15 16:34:34 -0400

    Started UI Redisign and OneRoster Models

---
+++ Models/Classes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDGenWebsite.Models
+{
+    public class Classes
+    {
+        public Guid SourcedID { get; set; }
+        public string Status { get; set; }
+        public string DateLastModified { get; set; }
+        public string Title { get; set; }
+        public Guid CourseSourcedID { get; set; }
+        public string ClassCode { get; set; }
+        public string ClassType { get; set; }
+        public string Location { get; set; }
+        public Guid SchoolSourcedID { get; set; }
+        public Guid TermSourcedID { get; set; }
+    }
+}
