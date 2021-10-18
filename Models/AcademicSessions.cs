030d5726b9da77e5b45e69d8c7dff1f987fa3933
Casey Lemieux <lemieuxcasey@gmail.com>
2021-10-17 20:10:50 -0400

    OneRoster Models Added

--- Models/AcademicSessions.cs
+++ Models/AcademicSessions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,16 @@ namespace IDGenWebsite.Models
 {
     public class AcademicSessions
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+        public Guid SourcedID { get; set; }
+        public string Status { get; set; }
+        public DateTime DateLastModified { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Guid ParentSourcedID { get; set; }
+        public int SchoolYear { get; set; }
     }
 }
--- Models/Classes.cs
+++ Models/Classes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,15 +8,24 @@ namespace IDGenWebsite.Models
 {
     public class Classes
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [System.ComponentModel.DataAnnotations.Key]
         public Guid SourcedID { get; set; }
         public string Status { get; set; }
-        public string DateLastModified { get; set; }
+        public DateTime DateLastModified { get; set; }
+        public string Metadata { get; set; }
         public string Title { get; set; }
-        public Guid CourseSourcedID { get; set; }
         public string ClassCode { get; set; }
         public string ClassType { get; set; }
         public string Location { get; set; }
-        public Guid SchoolSourcedID { get; set; }
-        public Guid TermSourcedID { get; set; }
+        public int MyProperty { get; set; }
+        public List<Grades> Grades { get; set; }
+        public List<Subjects> Subjects { get; set; }
+        public Guid Course { get; set; }
+        public Guid School { get; set; }
+        public List<AcademicSessions> Terms { get; set; }
+        public List<SubjectCodes> SubjectCodes { get; set; }
+        public List<Periods> Periods { get; set; }
+        public List<Resources> Resources { get; set; }
     }
 }
